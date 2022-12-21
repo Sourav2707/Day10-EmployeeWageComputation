a8640c7d8896fb780e2ce2be6426b09fe73d6e95
Sourav2707 <smartsourav33@gmail.com>
2022-12-21 19:22:31 +0530

    UC1 checking whether the employee is present or absent

--- src/com/day8_employee_wage/EmployeeWage.java
+++ src/com/day8_employee_wage/EmployeeWage.java
@@ -1,7 +1,20 @@
 package com.day8_employee_wage;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
+        int fullTime = 1;
+        Random rnd = new Random();
+        int attendCheck = rnd.nextInt(0,2);
+        if(attendCheck == fullTime)
+        {
+            System.out.println("The Employee is present");
+        }
+        else
+        {
+            System.out.println("The Employee is absent");
+        }
     }
 }
